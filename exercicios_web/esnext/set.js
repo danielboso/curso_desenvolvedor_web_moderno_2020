d3f7a7bb984dac865de32dcf91eb352d49143127
danielboso <bosodaniel@hotmail.com>
2020-04-08 01:08:54 -0300

    esnext

---
+++ exercicios_web/esnext/set.js
@@ -0,0 +1,18 @@
+// não aceita repetição/não indexada
+const times = new Set()
+times.add('Vasco')
+times.add('Vasco').add('Palmeiras').add('Corinthians')
+times.add('Flamengo')
+times.add('Vasco')
+
+console.log(times)
+console.log(times.size)
+console.log(times.has('vasco'))
+console.log(times.has('Vasco'))
+console.log(times.has('Flamengo'))
+
+const nomes = ['Raquel', 'Lucas', 'Julia', 'Lucas']
+const nomesSet = new Set(nomes)
+console.log(nomesSet)
+
+
